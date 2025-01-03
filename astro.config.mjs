be0e90373ef4c05b861d31c2812bafe6f832397e
Dan Forsberg <dforsber@gmail.com>
2025-01-03 09:05:43 +0200

    formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ export default defineConfig({
       },
     },
     css: {
-      devSourcemap: process.env.NODE_ENV === 'development'
+      devSourcemap: process.env.NODE_ENV === "development",
     },
     ssr: {
       noExternal: ["bootstrap"],
